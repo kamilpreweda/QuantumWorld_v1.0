30a1663f826ca2f5ee39e7c2f65d6aea558bc12d
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-15 21:11:04 +0200

    Added 'OverviewViewModel'

---
+++ QuantumWorld_v1.0/ViewModel/OverviewViewModel.cs
@@ -0,0 +1,27 @@
+﻿using QuantumWorld_v1._0.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumWorld_v1._0.ViewModel
+{
+    public class OverviewViewModel : ObservableObject
+    {
+        private PlayerModel _player;
+        public PlayerModel Player { get => _player;
+            set
+            {
+                _player = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public OverviewViewModel(PlayerModel player)
+        {
+            Player = player;
+        }
+    
+    }
+}

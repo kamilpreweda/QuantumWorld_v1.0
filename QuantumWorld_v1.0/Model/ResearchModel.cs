dc992a00b8d1b7a26c3c3644734208209795a93e
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-15 15:52:00 +0200

    Added 'ResearchModel' class

---
+++ QuantumWorld_v1.0/Model/ResearchModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumWorld_v1._0.Model
+{
+    public class ResearchModel
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public int Level { get; private set; }
+
+        public ResourceModel[] Cost { get; private set; }
+
+        public ResearchModel(string name, string description, int level, ResourceModel[] cost)
+        {
+            Name = name;
+            Description = description;
+            Level = level;
+            Cost = cost;
+        }
+    }
+}

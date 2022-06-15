a92f888f0bbca3cd577caa00155df02805b0cc52
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-15 15:20:20 +0200

    Added 'ResourceModel' class

---
+++ QuantumWorld_v1.0/Model/ResourceModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumWorld_v1._0.Model
+{
+    public class ResourceModel
+    {
+        public string Name { get; private set; }
+        public int Value { get; private set; }
+
+        public ResourceModel(string name, int value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+}

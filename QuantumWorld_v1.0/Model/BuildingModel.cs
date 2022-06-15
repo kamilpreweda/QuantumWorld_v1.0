9c2b204163c39e3b206cabc9e31f2448687697d2
Kamil Preweda <preweda.kamil@o2.pl>
2022-06-15 15:18:55 +0200

    Added 'BuildingModel' class

---
+++ QuantumWorld_v1.0/Model/BuildingModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumWorld_v1._0.Model
+{
+    public class BuildingModel
+    {
+        public int Level { get; private set; }
+        public string Name { get; private set; }
+
+        public ResourceModel[] Cost { get; private set }
+
+        public BuildingModel(string name, ResourceModel[] cost, int level)
+        {
+            Name = name;
+            Cost = cost;
+            Level = level;
+        }
+    }
+}

1946e6aed65ecf1fe6a9a375d26408fa69fd4234
Kamil Preweda <preweda.kamil@o2.pl>
2022-07-07 18:28:47 +0200

    Added ShipModel

---
+++ QuantumWorld_v1.0/Model/ShipModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantumWorld_v1._0.Model
+{
+    public class ShipModel
+    {
+        public string Name { get; }
+        public int Count { get; set; }
+        public int HealthPoints { get; set; }
+        public int AttackPower { get; set; }    
+    }
+}
